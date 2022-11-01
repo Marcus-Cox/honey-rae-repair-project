fef3dbbd2de37d674e595c4f10a73afd6740c9fa
Marcus-Cox <marcus.cox92@gmail.com>
2022-11-01 11:55:56 -0500

    chapter 6 done

--- honeyrae/urls.py
+++ honeyrae/urls.py
@@ -16,10 +16,16 @@ Including another URLconf
 from django.contrib import admin
 from django.conf.urls import include
 from django.urls import path
+from rest_framework import routers
 from repairsapi.views import register_user, login_user
+from repairsapi.views import CustomerView
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'customers', CustomerView, 'customer')
 
 urlpatterns = [
     path('register', register_user),
     path('login', login_user),
     path('admin/', admin.site.urls),
+    path('', include(router.urls)),
 ]
