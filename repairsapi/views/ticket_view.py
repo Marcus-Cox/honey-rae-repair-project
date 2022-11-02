6ab060fd85e28c5dbe68cd5fe13231d6bfe335b2
Marcus-Cox <marcus.cox92@gmail.com>
2022-11-02 11:13:10 -0500

    chapter 12 done

--- repairsapi/views/ticket_view.py
+++ repairsapi/views/ticket_view.py
@@ -4,6 +4,8 @@ from rest_framework.viewsets import ViewSet
 from rest_framework.response import Response
 from rest_framework import serializers, status
 from repairsapi.models import ServiceTicket
+from repairsapi.models import Employee
+from repairsapi.models import Customer
 
 
 class ServiceTicketView(ViewSet):
@@ -60,6 +62,25 @@ class ServiceTicketView(ViewSet):
         serialized = ServiceTicketSerializer(new_ticket, many=False)
 
         return Response(serialized.data, status=status.HTTP_201_CREATED)
+
+    def update(self, request, pk=None):
+
+        # Select the Targeted Ticket usking pk
+        ticket = ServiceTicket.objects.get(pk=pk)
+
+        # Get the emplpyee id from the client request
+        employee_id = request.data['employee']
+
+        # select the emplpyee from the database using that id
+        assigned_employee = Employee.objects.get(pk=employee_id)
+
+        # Assign that Employee instance to the employee property of the ticket
+        ticket.employee = assigned_employee
+
+        # Save the updated ticket
+        ticket.save()
+
+        return Response(None, status=status.HTTP_204_NO_CONTENT)
 
 
 class ServiceTicketSerializer(serializers.ModelSerializer):
